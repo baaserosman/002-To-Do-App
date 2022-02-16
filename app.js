ef36df96fbedfca5d274325614a31fb03d28a918
baaserosman <baaser.osman@gmail.com>
2022-02-16 23:35:55 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+// function check() {
+//   document.getElementsByClassName("myCheck").checked = true;
+// }
+
+// function uncheck() {
+//   document.getElementsByClassName("myCheck").checked = false;
+// }
+
+document.querySelector(".check1").onclick = () => {
+  document.querySelector(".item1").style.backgroundColor = "#589A8D";
+  document.querySelector(".item1").style.textDecoration = "line-through";
+  document.querySelector(".item1").style.color = "#eb6464";
+};
+
+document.querySelector(".check2").onclick = () => {
+  document.querySelector(".item2").style.backgroundColor = "#589A8D";
+  document.querySelector(".item2").style.textDecoration = "line-through";
+  document.querySelector(".item2").style.color = "#eb6464";
+};
+document.querySelector(".check3").onclick = () => {
+  document.querySelector(".item3").style.backgroundColor = "#589A8D";
+  document.querySelector(".item3").style.textDecoration = "line-through";
+  document.querySelector(".item3").style.color = "#eb6464";
+};
+document.querySelector(".check4").onclick = () => {
+  document.querySelector(".item4").style.backgroundColor = "#589A8D";
+  document.querySelector(".item4").style.color = "#eb6464";
+  document.querySelector(".item4").style.textDecoration = "line-through";
+  document.querySelector(".item4").style.color = "#eb6464";
+};
+document.querySelector(".check5").onclick = () => {
+  document.querySelector(".item5").style.backgroundColor = "#589A8D";
+  document.querySelector(".item5").style.textDecoration = "line-through";
+  document.querySelector(".item5").style.color = "#eb6464";
+};
